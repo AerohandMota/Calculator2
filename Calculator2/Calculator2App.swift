c3ad93967e2dbba0fac6f24cdc10dc9c5d3eca8e
MOTOYAMA Takefumi <bamboooo0130@gmail.com>
2020-09-17 20:41:59 +0900

    Initial Commit

---
+++ Calculator2/Calculator2App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator2App.swift
+//  Calculator2
+//
+//  Created by 本山武文 on 2020/09/17.
+//
+
+import SwiftUI
+
+@main
+struct Calculator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator2
+//
+//  Created by 本山武文 on 2020/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
